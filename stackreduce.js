ce2a73a65cb570e4939714f3986a2617dae4ca6e
John Kolovos <yannis.kolovos@gmail.com>
2013-08-07 05:08:16 +1000

    Keep it simple

--- stackreduce.js
+++ stackreduce.js
@@ -1,23 +1,44 @@
-/* 
-StackReduce API Library
-Version:  0.1.rc1 BETA
-Requires: jQuery 
-*/
+/******************************************************************
+	StackReduce API Wrapper Library
+	Version:  0.1.rc1 BETA
+	Requires: jQuery 1.10.1
+	Author: Yannis Kolovos
+*******************************************************************/
 
+
+if (typeof jQuery == 'undefined') {
+    function getScript(url, success) {
+        var script = document.createElement('script');
+        script.src = url;
+        var head = document.getElementsByTagName('head')[0],
+            done = false;
+        script.onload = script.onreadystatechange = function() {
+            if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
+                done = true;
+                success();
+                script.onload = script.onreadystatechange = null;
+                head.removeChild(script);
+            };
+        };
+        head.appendChild(script);
+    };
+
+    getScript('http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js', function() {
+		console.log("Loaded")
+    });
+} 
+	
+
+/******************************************************************
+	Main StackReduce Object
+	Keep it simple!
+*******************************************************************/
 var StackReduce = (function (sr) {
-
-	sr.helpers = {}
-	sr.helpers.humanize = function(property) {
-	  return property.replace(/_/g, ' ')
-	      .replace(/(\w+)/g, function(match) {
-	        return match.charAt(0).toUpperCase() + match.slice(1);
-	      });
-	};
-		
+	sr.end_point = "http://www.stackreduce.com/api/v1/stacks.json"
 	sr.push = function (data){
 	$.ajax({
 		type: "POST",
-		url: this.url, 
+		url: this.end_point, 
 		dataType: "json",
 		data:  {
 		_method:'POST',
